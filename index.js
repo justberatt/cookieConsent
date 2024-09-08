9a8a4b234853fbf88ea28d590077422cea28cb88
justberatt <berat.sakipi@gmail.com>
2024-09-08 23:59:12 +0200

    added authentication

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-/*
-Challenge:
-1. Take control of the close button.
-2. Use an event listener to set the display 
-   property of the modal to 'none' when the
-   close button is clicked.
-*/
-
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
  
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -15,4 +9,34 @@ setTimeout(function(){
 
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
+})
+
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+    
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>` 
+    
+    setTimeout(function(){
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
+    }, 1500)
+    
+    
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+    `
+    }, 3000)
+  
 })
